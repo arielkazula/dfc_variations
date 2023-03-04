84625fd6aff7a4d586f88c724043dfa2e0e9e315
menuchaben <menuchaben@nvram001.localdomain>
2023-03-04 18:53:06 +0200

    hange run script and fix pinning to single numa node.

--- flat_dfc/run.sh
+++ flat_dfc/run.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 # Create a Makefile with CMake, compile and run DFC
 
-make clean ; rm -rf CMakeCache.txt /dev/shm/* CMakeFiles/ Makefile dfc  ; cmake  . ; make ; >> /dev/null 
-./dfc
-make clean ; rm -rf CMakeCache.txt /dev/shm/* CMakeFiles/ Makefile dfc  ; cmake -DCMAKE_CXX_FLAGS="-DSINGLE_NUMA"  . ; make ; >> /dev/null 
-./dfc
-make clean ; rm -rf CMakeCache.txt /dev/shm/* CMakeFiles/ Makefile dfc  ; cmake -DCMAKE_CXX_FLAGS="-DYIELD_WAIT"  . ; make ;  >> /dev/null 
-./dfc
-make clean ; rm -rf CMakeCache.txt /dev/shm/* CMakeFiles/ Makefile dfc  ; cmake -DCMAKE_CXX_FLAGS="-DYIELD_COMBINER_DONE"  . ; make ; >> /dev/null 
-./dfc
+#make clean ; rm -rf CMakeCache.txt /dev/shm/* CMakeFiles/ Makefile dfc  ; cmake  . ; make ; >> /dev/null 
+#./dfc
+#make clean ; rm -rf CMakeCache.txt /dev/shm/* CMakeFiles/ Makefile dfc  ; cmake -DCMAKE_CXX_FLAGS="-DSINGLE_NUMA"  . ; make ; >> /dev/null 
+#./dfc
+#make clean ; rm -rf CMakeCache.txt /dev/shm/* CMakeFiles/ Makefile dfc  ; cmake -DCMAKE_CXX_FLAGS="-DYIELD_WAIT"  . ; make ;  >> /dev/null 
+#./dfc
+#make clean ; rm -rf CMakeCache.txt /dev/shm/* CMakeFiles/ Makefile dfc  ; cmake -DCMAKE_CXX_FLAGS="-DYIELD_COMBINER_DONE"  . ; make ; >> /dev/null 
+#./dfc
 make clean ; rm -rf CMakeCache.txt /dev/shm/* CMakeFiles/ Makefile dfc  ; cmake -DCMAKE_CXX_FLAGS="-DTHREAD_PIN"  . ; make ;  >> /dev/null 
 ./dfc
-make clean ; rm -rf CMakeCache.txt /dev/shm/* CMakeFiles/ Makefile dfc  ; cmake -DCMAKE_CXX_FLAGS="-DYIELD_COMBINER_CPU"  . ; make ; >> /dev/null 
-./dfc
-make clean ; rm -rf CMakeCache.txt /dev/shm/* CMakeFiles/ Makefile dfc  ; cmake -DCMAKE_CXX_FLAGS="-DSINGLE_NUMA -DYIELD_WAIT"  . ; make ; >> /dev/null 
-./dfc
-make clean ; rm -rf CMakeCache.txt /dev/shm/* CMakeFiles/ Makefile dfc  ; cmake -DCMAKE_CXX_FLAGS="-DSINGLE_NUMA -DYIELD_WAIT -DYIELD_COMBINER_DONE"  . ; make ; >> /dev/null 
+#make clean ; rm -rf CMakeCache.txt /dev/shm/* CMakeFiles/ Makefile dfc  ; cmake -DCMAKE_CXX_FLAGS="-DYIELD_COMBINER_CPU"  . ; make ; >> /dev/null 
+#./dfc
+#make clean ; rm -rf CMakeCache.txt /dev/shm/* CMakeFiles/ Makefile dfc  ; cmake -DCMAKE_CXX_FLAGS="-DSINGLE_NUMA -DYIELD_WAIT"  . ; make ; >> /dev/null 
+#./dfc
+#make clean ; rm -rf CMakeCache.txt /dev/shm/* CMakeFiles/ Makefile dfc  ; cmake -DCMAKE_CXX_FLAGS="-DSINGLE_NUMA -DYIELD_WAIT -DYIELD_COMBINER_DONE"  . ; make ; >> /dev/null 
+#./dfc
+make clean ; rm -rf CMakeCache.txt /dev/shm/* CMakeFiles/ Makefile dfc  ; cmake -DCMAKE_CXX_FLAGS="-DTHREAD_PIN -DSINGLE_NUMA -DYIELD_WAIT -DYIELD_COMBINER_DONE"  . ; make ; >> /dev/null 
 ./dfc
